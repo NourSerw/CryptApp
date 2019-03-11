a16b1ebcb4bb62163043da2701f73c0aab7a66c0
NourSerw <nourserw@hotmail.com>
2019-03-11 13:36:05 +0200

    Add project files.

---
+++ CryptApp/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CryptApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputCrypt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.result_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insert Text here";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(29, 90);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(100, 20);
+            this.inputText.TabIndex = 2;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(26, 181);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(25, 13);
+            this.result.TabIndex = 3;
+            this.result.Text = "Null";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Insert Text here";
+            // 
+            // inputCrypt
+            // 
+            this.inputCrypt.Location = new System.Drawing.Point(213, 90);
+            this.inputCrypt.Name = "inputCrypt";
+            this.inputCrypt.Size = new System.Drawing.Size(100, 20);
+            this.inputCrypt.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // result_2
+            // 
+            this.result_2.AutoSize = true;
+            this.result_2.Location = new System.Drawing.Point(210, 181);
+            this.result_2.Name = "result_2";
+            this.result_2.Size = new System.Drawing.Size(25, 13);
+            this.result_2.TabIndex = 7;
+            this.result_2.Text = "Null";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 254);
+            this.Controls.Add(this.result_2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.inputCrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CryptoFunko";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputCrypt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label result_2;
+    }
+}
+
---
+++ CryptApp/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptApp
+{
+    public partial class Form1 : Form
+    {
+
+        public char[] EncryptIn;
+        public char[] EncryptOut;
+        public char[] decryptIn;
+        public char[] decryptOut;
+        public string result_text;
+        public char[,] cryptKey = new char[,]
+            {
+                {'q','a'},
+                {'a','z'},
+                {'w','s'},
+                {'s','x'},
+                {'z','w'},
+                {'x','e'},
+                {'e','d'},
+                {'d','c'},
+                {' ','@'},
+                {'i','k'},
+                {'m', 'o'},
+                {'h','n'},
+                {'c','t'},
+                {'r','f'},
+                {'f','v'},
+                {'b','u'},
+                {'o','l'},
+                {'y','h'},
+                {'n','i'},
+                {'k','<'},
+                {'p',';'},
+                {'u','j'},
+                {'l','.'},
+                {'t','g'}
+            };
+        public char[] number_Codes = new char[10] { ')', '!', '@', '#', '$', '%', '^', '&', '*', '(' };
+            
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result_text = null;
+            EncryptIn = ((inputText.Text).ToLower()).ToCharArray();
+            Array.Resize(ref EncryptOut, EncryptIn.Length);
+            int w = cryptKey.GetLength(0);
+            for (int i = 0; i < EncryptIn.Length; i++)
+            {
+                for (int z = 0; z < w; z++)
+                {
+                    if (cryptKey[z, 0].Equals(EncryptIn[i]))
+                    {
+                        EncryptOut[i] = cryptKey[z, 1];
+                    }
+                }
+            }
+            Random random = new Random();
+            
+            for (int i = 0; i < EncryptOut.Length; i++)
+            {
+                int random_Number = random.Next(1, 135);
+                if (random_Number % 2 == 0)
+                {
+                    result_text += (Char.ToString(EncryptOut[i])).ToUpper();
+                }
+                else
+                {
+                    result_text += Char.ToString(EncryptOut[i]);
+                }
+            }
+            result.Text = result_text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result_text = null;
+            decryptIn = ((inputCrypt.Text).ToLower()).ToCharArray();
+            Array.Resize(ref decryptOut, decryptIn.Length);
+            int w = cryptKey.GetLength(0);
+            for (int i = 0; i < decryptIn.Length; i++)
+            {
+                for (int z = 0; z < w; z++)
+                {
+                    if (cryptKey[z,1].Equals(decryptIn[i]))
+                    {
+                        decryptOut[i] = cryptKey[z,0];
+                    }
+                }
+            }
+            for (int i = 0; i < decryptOut.Length; i++)
+            {
+                result_text += Char.ToString(decryptOut[i]);
+            }
+            result_2.Text = result_text;
+        }
+    }
+}
